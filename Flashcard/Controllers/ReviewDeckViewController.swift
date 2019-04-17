e068f718dcd7fe11f87ba5e101b2b8a1e4db239b
shaunjacobsen <shaun.d.jacobsen@gmail.com>
2019-04-16 23:36:14 -0500

    Cards can be swiped

--- Flashcard/Controllers/ReviewDeckViewController.swift
+++ Flashcard/Controllers/ReviewDeckViewController.swift
@@ -9,29 +9,44 @@
 import UIKit
 
 let screenSize = UIScreen.main.bounds
+let SEPARATOR_DISTANCE = 8
+let MAX_BUFFER_SIZE = 3
 
 class ReviewDeckViewController: UIViewController {
   
   var selectedDeck: Deck?
+  var allSwipeableCards: [SwipeableCard] = []
+  var swipeableCards: [SwipeableCard] = []
+  var currentIndex = 0
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    if let deck = selectedDeck {
+      let capCount = (deck.cards.count > MAX_BUFFER_SIZE) ? MAX_BUFFER_SIZE : deck.cards.count
+      for (i, card) in deck.cards.enumerated() {
+        let newCard = createCard(from: card)
+        allSwipeableCards.append(newCard)
+        if i < capCount {
+          swipeableCards.append(newCard)
+        }
+      }
+      
+      for (i, _) in swipeableCards.enumerated() {
+        if i > 0 {
+          view.insertSubview(swipeableCards[i], belowSubview: swipeableCards[i - 1])
+        } else {
+          view.addSubview(swipeableCards[i])
+        }
+      }
+      
+      animateCardAfterSwiping()
+      
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    if let deck = selectedDeck {
-      if let firstCard = deck.cards.first {
-        let newCard = createCard(from: firstCard)
-        view.addSubview(newCard)
-        
-        let safeAreaGuide = view.safeAreaLayoutGuide
-        NSLayoutConstraint.activate([
-          newCard.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor, constant: 40),
-          newCard.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 40),
-          newCard.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: 40),
-          newCard.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor, constant: 80),
-          ])
-      }
-    }
-    
   }
   
   //
@@ -39,7 +54,7 @@ class ReviewDeckViewController: UIViewController {
   //
   
   func createCard(from card: Card) -> SwipeableCard {
-    let cardView = SwipeableCard() as SwipeableCard
+    let cardView = SwipeableCard()
     cardView.frame = CGRect(x: 40, y: 100, width: screenSize.width - 80, height: screenSize.height - 220)
     cardView.mainLabel.text = card.questionText
     cardView.secondaryLabel.text = card.questionNotes
@@ -47,8 +62,71 @@ class ReviewDeckViewController: UIViewController {
     cardView.layer.shadowOpacity = 0.11
     cardView.layer.shadowOffset = CGSize(width: 0, height: 3)
     cardView.layer.shadowRadius = 14
+    cardView.delegate = self
+    cardView.isUserInteractionEnabled = false
+    cardView.addGestureRecognizer(cardView.panGestureRecognizer)
     
     return cardView
   }
+  
+  func dismissIfNoFurtherCards() {
+    if swipeableCards.isEmpty {
+      self.dismiss(animated: true, completion: nil)
+    }
+  }
+  
+  func removeCardAndAppendNew() {
+    swipeableCards.remove(at: 0)
+    currentIndex = currentIndex + 1
+    
+    if (currentIndex + swipeableCards.count) < allSwipeableCards.count {
+      let card = allSwipeableCards[currentIndex + swipeableCards.count]
+      var frame = card.frame
+      frame.origin.y = CGFloat(MAX_BUFFER_SIZE * SEPARATOR_DISTANCE)
+      card.frame = frame
+      swipeableCards.append(card)
+      view.insertSubview(swipeableCards[MAX_BUFFER_SIZE - 1], belowSubview: swipeableCards[MAX_BUFFER_SIZE - 2])
+    }
+    animateCardAfterSwiping()
+  }
+  
+  func animateCardAfterSwiping() {
+    for (i, card) in swipeableCards.enumerated() {
+      UIView.animate(withDuration: 0.5, animations: {
+        if i == 0 {
+          card.isUserInteractionEnabled = true
+        }
+        var frame = card.frame
+        frame.origin.y = 100 + (CGFloat(i * SEPARATOR_DISTANCE))
+        card.frame = frame
+      })
+    }
+  }
 
+}
+
+extension ReviewDeckViewController: SwipeableCardDelegate {
+  func cardMovement(card: SwipeableCard) {
+    animateCardAfterSwiping()
+    removeCardAndAppendNew()
+    dismissIfNoFurtherCards()
+  }
+  
+  func cardWentLeft(card: SwipeableCard) {
+    print("Card went left")
+  }
+  
+  func cardWentRight(card: SwipeableCard) {
+    print("Card went right")
+  }
+  
+  func cardWentUp(card: SwipeableCard) {
+    print("Card went up and away")
+  }
+  
+  func currentCardStatus(card: SwipeableCard, distance: CGFloat) {
+    
+  }
+  
+  
 }
--- Flashcard/Views/CardView/SwipeableCard.swift
+++ Flashcard/Views/CardView/SwipeableCard.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
+let THRESHOLD_MARGIN = (UIScreen.main.bounds.size.width / 2) * 0.5
+let SCALE_STRENGTH: CGFloat = 4
+let SCALE_RANGE: CGFloat = 0.90
+
 protocol SwipeableCardDelegate: NSObjectProtocol {
-  func dragCardLeft(card: SwipeableCard)
-  func dragCardRight(card: SwipeableCard)
-  func dragCardUp(card: SwipeableCard)
+  func cardMovement(card: SwipeableCard)
+  func cardWentLeft(card: SwipeableCard)
+  func cardWentRight(card: SwipeableCard)
+  func cardWentUp(card: SwipeableCard)
   func currentCardStatus(card: SwipeableCard, distance: CGFloat)
 }
 
 class SwipeableCard: UIView {
+  
+  var xCenter: CGFloat = 0.0
+  var yCenter: CGFloat = 0.0
+  var originalPoint = CGPoint.zero
+  lazy var generator = UIImpactFeedbackGenerator(style: .light)
+  
+  lazy var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handleDragging))
+  
+  weak var delegate: SwipeableCardDelegate?
   
   @IBOutlet weak var mainLabel: UILabel!
   @IBOutlet weak var secondaryLabel: UILabel!
@@ -34,6 +48,90 @@ class SwipeableCard: UIView {
     super.awakeFromNib()
   }
   
+  @objc func handleDragging(_ gestureRecognizer: UIPanGestureRecognizer) {
+    xCenter = gestureRecognizer.translation(in: self).x
+    yCenter = gestureRecognizer.translation(in: self).y
+    switch gestureRecognizer.state {
+    case .began:
+      originalPoint = self.center;
+      break;
+    case .changed:
+      let rotationStrength = min(xCenter / UIScreen.main.bounds.size.width, 1)
+      let rotationAngel = .pi/8 * rotationStrength
+      let scale = max(1 - abs(rotationStrength) / SCALE_STRENGTH, SCALE_RANGE)
+      center = CGPoint(x: originalPoint.x + xCenter, y: originalPoint.y + yCenter)
+//      print("xCenter: \(xCenter)")
+//      print("THRESHOLD_MARGIN: \(THRESHOLD_MARGIN)")
+//
+//      if Int(xCenter) == Int(THRESHOLD_MARGIN) || Int(xCenter) == Int(-THRESHOLD_MARGIN) {
+//        generator.impactOccurred()
+//      }
+      let transforms = CGAffineTransform(rotationAngle: rotationAngel)
+      let scaleTransform: CGAffineTransform = transforms.scaledBy(x: scale, y: scale)
+      self.transform = scaleTransform
+      break;
+      
+    case .ended:
+      afterSwipeAction()
+      break;
+      
+    case .possible:break
+    case .cancelled:break
+    case .failed:break
+    }
+  }
   
+  func afterSwipeAction() {
+    if xCenter > THRESHOLD_MARGIN {
+      cardWentLeft()
+    }
+    else if xCenter < -THRESHOLD_MARGIN {
+      cardWentRight()
+    } else if yCenter < (UIScreen.main.bounds.size.height / 2) * 0.5 {
+      cardWentUp()
+    } else {
+      UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1.0, options: [], animations: {
+        self.center = self.originalPoint
+        self.transform = CGAffineTransform(rotationAngle: 0)
+        self.delegate?.currentCardStatus(card: self, distance: 0)
+      })
+    }
+  }
+  
+  func cardWentLeft() {
+    let finishPoint = CGPoint(x: 2 * xCenter + originalPoint.x, y: frame.size.height * 2)
+    UIView.animate(withDuration: 0.5, animations: {
+      self.center = finishPoint
+    }, completion: {(_) in
+      self.removeFromSuperview()
+    })
+    // handle card actions here
+    delegate?.cardMovement(card: self)
+    delegate?.cardWentLeft(card: self)
+  }
+  
+  func cardWentRight() {
+    let finishPoint = CGPoint(x: 2 * xCenter + originalPoint.x, y: frame.size.height * 2)
+    UIView.animate(withDuration: 0.5, animations: {
+      self.center = finishPoint
+    }, completion: {(_) in
+      self.removeFromSuperview()
+    })
+    // handle card actions here
+    delegate?.cardMovement(card: self)
+    delegate?.cardWentRight(card: self)
+  }
+  
+  func cardWentUp() {
+    let finishPoint = CGPoint(x: xCenter, y: -frame.size.height * 2)
+    UIView.animate(withDuration: 0.5, animations: {
+      self.center = finishPoint
+    }, completion: {(_) in
+      self.removeFromSuperview()
+    })
+    // handle card actions here
+    delegate?.cardMovement(card: self)
+    delegate?.cardWentUp(card: self)
+  }
 
 }
